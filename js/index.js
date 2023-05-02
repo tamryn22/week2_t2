8079295a97494f47263534110479e52d325a1359
tamryn22 <125568338+tamryn22@users.noreply.github.com>
2023-05-02 13:34:09 +0200

    Variables
    homework ex 1,2,3,4
    Learnt: let, var , const

---
+++ js/index.js
@@ -0,0 +1,61 @@
+//ex 1
+
+
+var firstName = "Tamryn";
+var lastName = "De lange";
+const subject = "Development";
+var age = 18;
+
+//console.log(firstName);
+//console.log(lastName);
+//console.log(subject);
+//console.log(age);
+
+let greeter = " Hello, my name is " + firstName + " " + lastName + ", I am " + age + " years old and I am studying " + subject
+console.log(greeter);
+
+//ex 2
+let numOne = 4;
+let numTwo = 20;
+let numThree = 2;
+let numFour = 4;
+let answer = 0;
+answer = numOne + numTwo / numThree * numFour
+console.log(answer); 
+
+//ex 3
+
+let susan = 25 * 0.26
+let timmy = 10 * 0.15
+let value= susan + timmy;
+
+//console.log(value)
+let win = "The Tax Man will claim R" + value +" from Timmy & Susan"
+console.log (win)
+//let answer = teamST + victory;
+
+//ex 4 no.1
+let josh =50;
+let tim = 52;
+let alfie = 70;
+let gwen = 73;
+
+//let value = josh + tim + alfie + gwen / 4 
+value = josh + tim + alfie + gwen /4
+
+let student = "The Student average is:" + value + "/100"
+console.log(student)
+
+//ex 4 no.2
+let updateMark = 7
+josh = josh + updateMark;
+console.log (josh)
+//just consoled this to show that it's working 
+tim = tim + updateMark;
+alfie = alfie + updateMark;
+gwen = gwen + updateMark;
+value =josh + tim + alfie + gwen / 4
+
+//let update = "The students updated average is: " value +"/100"
+//console.log (update)
+//im not sure why but it won't console the let update, I've tried changing the word to many other things to see if it would console but it keep saying error
